c87276f2366ffd322596b09bdf4149b03e2cb869
Vlad <vladzaharchev@gmail.com>
2019-10-17 14:36:06 +0200

    second task

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var money= parseInt(prompt("Ваш бюджет на месяц?"));
+var money= +(prompt("Ваш бюджет на месяц?"));
 var time= prompt("Введите дату в формате YYYY-MM-DD");
 
 var appData = {
@@ -12,12 +12,29 @@ var appData = {
     savings: false
 };
 
-let a1= prompt("Введите обязательную статью расходов вэтом месяце"),
-    a2 = prompt("Во сколько обойдётся?"),
-    a3 = prompt("Введите обязательную статью расходов вэтом месяце"),
-    a4 = prompt("Во сколько обойдётся?");
+for(let i = 0; i < 2; i++){
+    let a= prompt("Введите обязательную статью расходов вэтом месяце"),
+    b = prompt("Во сколько обойдётся?");
 
-appData.expences.a1 = a2;
-appData.expences.a3 = a4;
+    if( (typeof(a)) === 'string' && (typeof(a) != null) && 
+    (a != '') && typeof(b) != null && a.length < 50){
+        appData.expences[a] = b;
+    } else {
+
+    }
+}
+/*
+let i =0;
+while(i<2){
+    //code
+    i++;
+} */
+
+/*
+let i = 0;
+do {
+    //code
+    i++;
+} while(i<2); */
 
 alert(appData.budget/30);
